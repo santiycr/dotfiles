2a220e2916411fba675b3c2c3790a1b13dd8383d
Santiago Suarez Ordoñez <santiycr@gmail.com>
2012-07-28 17:17:45 -0700

    adding bin scripts setup

--- install.py
+++ install.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import sys
 import os
 import shutil
@@ -6,25 +7,29 @@ import shutil
 DOTFILES_PATH = os.path.dirname(os.path.realpath(__file__))
 HOME_PATH = os.environ['HOME']
 
+
 def safe_ln(source, dest):
     source = "%s/%s" % (DOTFILES_PATH, source)
     dest = "%s/%s" % (HOME_PATH, dest)
     if os.path.islink(dest):
-        print "Existing link in %s to %s found. Unlinking." % (dest, os.path.realpath(dest))
+        print ("Existing link in %s to %s found. Unlinking."
+                % (dest, os.path.realpath(dest)))
         os.unlink(dest)
     elif os.path.isfile(dest) or os.path.isdir(dest):
         print "Already existing config found. Moving up to %s.orig" % dest
-        shutil.move(dest, dest+".orig")
+        shutil.move(dest, dest + ".orig")
     os.symlink(source, dest)
+
 
 def safe_mkdir(dest):
     dest = "%s/%s" % (DOTFILES_PATH, dest)
     if not os.path.isdir(dest):
         os.mkdir(dest)
 
+
 installs = sys.argv[1:]
 if installs == ['all']:
-    installs = ['vim', 'tmux', 'zsh', 'git']
+    installs = ['vim', 'tmux', 'zsh', 'git', 'bin']
 
 for install in installs:
     print "installing %s" % install
@@ -41,5 +46,9 @@ for install in installs:
         safe_ln('zsh/zshrc', '.zshrc')
     elif install == 'git':
         safe_ln('git/gitconfig', '.gitconfig')
+    elif install == 'bin':
+        safe_mkdir("bin")
+        for script in os.listdir(DOTFILES_PATH + '/bin'):
+            safe_ln(script, 'bin/' + os.path.basename(script))
     else:
         print "Invalid install requested: %s" % install
